58bff52d440c591875c764b2794c14fd8eecb0b6
IcaroM-CdC <icaro.moreira@protonmail.com>
2025-01-13 17:48:06 -0300

    [Icaro] adjusts

--- project/src/java/messages/Messages.java
+++ project/src/java/messages/Messages.java
@@ -5,7 +5,7 @@ import project.src.java.userInterface.ValidParameters;
 public class Messages {
 	public static String HELP = String.format("""
 		
-		batch execution
+		batch execution (work in progress)
 			read_settings   [filename]  read all execution settings contained in json file
 			run_settings 			    run all execution cases readed using read_settings command
 		manual execution
@@ -19,12 +19,13 @@ public class Messages {
 			%s  [options]   generate the inference design in verilog using conditional constructs with pipeline
 			%s  [options]   generate the inference design in verilog using multiplexers
 			%s   [options]   generate the inference design in verilog using boolean equations
+			%s  [options]   generate the inference design in verilog using multiple tables
 				-p			[value] 	defines the precision of the IEEE375 float point representation
 				 \u21B3 half:   16 bits
 				 \u21B3 normal: 32 bits
 				 \u21B3 double: 64 bits
 				
-			%s   [options]   generate the inference design in verilog using validation tables
+			%s   [options]   generate the inference design in verilog using validation tables (work in progress)
 				-tbw					defines the threshold bitwidth i.e the bitwidth of the values who are compared
 				-ibw					defines the node index bitwidth
 				-cbw					defines the bitwidth of the column who represent one dataset column
@@ -36,6 +37,7 @@ public class Messages {
 			ValidParameters.START_IF_PIPELINED_INFERENCE,
 			ValidParameters.START_MUX_INFERENCE,
 			ValidParameters.START_EQUATION_INFERENCE,
+			ValidParameters.START_PARALLEL_TABLE_INFERENCE,
 			ValidParameters.START_TABLE_INFERENCE
 	);
 }
